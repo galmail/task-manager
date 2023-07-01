fd0a06fb22ae399358906483b45a205918c636bd
Gal Dubitski <galmail@gmail.com>
2023-07-02 00:08:22 +0300

    topbar unit tests

---
+++ src/components/TopBar/TopBar.test.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { screen, render, act } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import TopBar from "./TopBar";
+
+const mockedUsedNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+  // ...(jest.requireActual("react-router-dom") as any),
+  useNavigate: () => mockedUsedNavigate,
+}));
+
+describe("TopBar component", () => {
+  it("renders the TopBar component", () => {
+    render(<TopBar title="the top bar title" />);
+    const title = screen.getByText(/the top bar title/);
+    expect(title).toBeInTheDocument();
+  });
+
+  it("renders the back button on TopBar component", async () => {
+    render(<TopBar title="the top bar title" canGoBack={true} />);
+    const backButton = screen.getByLabelText("back");
+    await userEvent.click(backButton);
+    expect(mockedUsedNavigate).toHaveBeenCalledWith(-1);
+  });
+
+  it("renders children in the TopBar component", () => {
+    render(
+      <TopBar title="the top bar title">
+        <button>My Cool Button</button>
+      </TopBar>
+    );
+    const button = screen.getByText(/My Cool Button/);
+    expect(button).toBeInTheDocument();
+  });
+});

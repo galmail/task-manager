1b7436105dffaf395388d83250bd05c8630af0de
Gal Dubitski <galmail@gmail.com>
2023-07-02 13:15:12 +0300

    cleanup

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 import type { Task } from "./types";
 
+const tasksEndpoint = "https://adam-deleteme.s3.amazonaws.com/tasks.json";
+
 export const fetchTasks = async (): Promise<Task[]> => {
-  console.log("fetching tasks");
-  const response = await axios.get<Task[]>(
-    "https://adam-deleteme.s3.amazonaws.com/tasks.json"
-  );
+  console.log("fetching tasks", tasksEndpoint);
+  const response = await axios.get<Task[]>(tasksEndpoint);
   return response.data;
 };
